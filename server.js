aacb9c537a9be312a8182cbbcac681ee704feb03
Mohit Chute <mohit12611@gmail.com>
2021-05-17 22:36:54 +0530

    initial commit

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ function authenticate(req, res, next) {
 }
 
 app.all('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://url-shortner-client.herokuapp.com/");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next()
   });
