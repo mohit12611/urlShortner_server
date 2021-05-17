7b980e4161550aac034e548fe59c13384d119b53
Mohit Chute <mohit12611@gmail.com>
2021-05-17 13:55:28 +0530

    initial commit

--- server.js
+++ server.js
@@ -166,7 +166,7 @@ app.get("/:id/:shortUrl", async (req, res) => {
     }
 })
 
-
-app.listen((process.env.PORT || 5000), () => {
-    console.log("listning");
+let PORT = (process.env.PORT || 5000);
+app.listen(PORT, () => {
+    console.log("listning on port" , PORT);
 });
